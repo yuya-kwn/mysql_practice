5497e3919595049e91dbb408a9535ba3c8c953d1
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-11-08 16:25:42 +0900

    initial commit

---
+++ script/initDb.sql
@@ -0,0 +1,12 @@
+create table sample1(
+    id int auto_increment,
+    col1 text,
+    col2 text,
+    primary key (id)
+) ;
+
+
+insert into sample1(col1, col2)values('aaa', '1111');
+insert into sample1(col1, col2)values('bbb', '2222');
+insert into sample1(col1, col2)values('ccc', '3333');
+insert into sample1(col1, col2)values('ddd', '4444');
---
+++ script/initShell.sh
@@ -0,0 +1,4 @@
+echo "### initalize start ####"
+mkdir /tmp/test
+touch /tmp/test/aaaa
+echo "### initialize end  ####"
